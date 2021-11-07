fbe9a7b37d6e3f15a194ea615d17734c606d7d39
Cyklan <37187056+Cyklan@users.noreply.github.com>
2021-11-07 17:03:56 +0100

    bullets deal damage

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,6 +7,8 @@ public class Bullet : MonoBehaviour
   public float speed = 10f;
   public float TTL = 20f;
 
+  public int damage = 1;
+
   void Update()
   {
     TTL -= Time.deltaTime;
@@ -22,7 +24,8 @@ public class Bullet : MonoBehaviour
     if (collision.isTrigger) return;
     if (collision.gameObject.tag == "Enemy")
     {
-      // deal damage and so on
+      // alter damage according to enchantments
+      collision.gameObject.GetComponent<Enemy>().TakeDamage(damage);
     }
     if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Wall")
     {
